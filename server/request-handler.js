ddca11b4b1d30b0865cfa15dcb9ef4be225d00f6
shams-ali <shams.ali0216@gmail.com>
2016-05-09 22:51:50 -0700

    Update file

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,16 +1,16 @@
-/*************************************************************
+// /*************************************************************
 
-You should implement your request handler function in this file.
+// You should implement your request handler function in this file.
 
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
+// requestHandler is already getting passed to http.createServer()
+// in basic-server.js, but it won't work as is.
 
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
+// You'll have to figure out a way to export this function from
+// this file and include it in basic-server.js so that it actually works.
 
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
+// *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
-**************************************************************/
+// **************************************************************/
 
 var q = require('querystring');
 var utils = require('util');
@@ -18,39 +18,21 @@ var utils = require('util');
 var body = {results: [] };
 
 var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
+  var method = request.method;
+  var url = request.url;
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = 'application/json';
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  // var method = request.method, url = request.url;
-
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  console.log('Serving request type ' + method + ' for url ' + url);
 
   // The outgoing status.
   var statusCode = 200;
-
-
 
   if (request.method === 'POST') {
     statusCode = 201;
   } else if (request.url !== '/classes/messages') {
     statusCode = 404;
   }
-
-  var headers = defaultCorsHeaders;
-
-  headers['Content-Type'] = 'application/json';
-
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
@@ -92,4 +74,22 @@ var defaultCorsHeaders = {
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
+
+
+/*************************************************************
+
+You should implement your request handler function in this file.
+
+requestHandler is already getting passed to http.createServer()
+in basic-server.js, but it won't work as is.
+
+You'll have to figure out a way to export this function from
+this file and include it in basic-server.js so that it actually works.
+
+*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
+
+**************************************************************/
+
+
+
 
